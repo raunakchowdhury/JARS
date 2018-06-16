d872a25945e85314c6a140546606b83af5186816
Angela <atom@stuy.edu>
2018-06-16 13:09:48 -0400

    Frontend basic stuff

--- app/App.js
+++ app/App.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">  
-          <h1 className="App-title">Photo Quizlet <small> by JARS </small></h1>
+        <div className="App-header">
+          <h1 className="App-title">Photo Quizlet <small>by JARS</small></h1>
         </div>
-        <p className="App-form"> 
-        </p>
-
-        </div>
+        
+      </div>
     );
   }
 }
-
 
 export default App;
